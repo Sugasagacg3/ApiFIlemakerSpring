f5cf48ca45222c26a1498688db8ef6688b864745
Tu nombre <jose@masqueideas.com>
2024-02-01 21:38:00 +0100

    Comit

---
+++ src/main/java/com/filemakerApi/apifilemkaer/FilemakerApiApplication.java
@@ -0,0 +1,13 @@
+package com.filemakerApi.apifilemkaer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemakerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemakerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filemakerApi/apifilemkaer/FilemakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filemakerApi.apifilemkaer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemakerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
